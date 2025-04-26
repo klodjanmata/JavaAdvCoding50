1d30d00590f8dbe9b6a08860c66274152176aebd
Klodjan Mata <klodjanmata0@gmail.com>
2025-04-26 13:45:29 +0200

    Exercise5 2

--- src/main/java/sda/javatirana48/Exercise5/SDAHashSet.java
+++ src/main/java/sda/javatirana48/Exercise5/SDAHashSet.java
@@ -1,6 +1,45 @@
 package sda.javatirana48.Exercise5;
 
-import java.util.HashSet;
+import java.util.LinkedList;
 
-public class SDAHashSet<E> extends HashSet<E> {
+public class SDAHashSet<E>{
+
+    private LinkedList<E>[] elements;
+    private static final int MAX_CAPACITY = 20;
+    private int size;
+
+    public SDAHashSet() {
+        elements = new LinkedList[MAX_CAPACITY];
+        size = 0;
+    }
+
+    public void add(E element) {
+        if (size >= MAX_CAPACITY){
+            System.out.println("SDAHashSet is full");
+            return;
+        }
+        elements[size].add(element);
+        size++;
+    }
+
+    public void remove(E element) {
+        if (elements[size].contains(element)){
+            elements[size].remove(element);
+            size--;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(E element) {
+        return elements[size].contains(element);
+    }
+
+    public void clear() {
+        size = 0;
+    }
+
+
 }
